97e99e34dbcff7ebb88174a3ec08b2bb6871c54d
1Macho <ozjuanpa@gmail.com>
2020-07-24 14:23:33 -0500

    Added async stuff

--- aidadventure.py
+++ aidadventure.py
@@ -1,15 +1,16 @@
 from gql import gql, Client, WebsocketsTransport
+from aobject import aobject
 
-class AIDungeonAdventure:
+class AIDungeonAdventure(aobject):
     public_id = ""
     id = ""
     client = None
     character_name = ""
 
-    def add_user_to_adventure(self):
+    async def add_user_to_adventure(self):
         if self.client.debug:
             print("Adding user to adventure {}".format(self.public_id))
-        result = self.client.request(
+        result = await self.client.request(
             '''
             mutation ($adventurePlayPublicId: String) {
                 addUserToAdventure(adventurePlayPublicId: $adventurePlayPublicId)
@@ -21,7 +22,7 @@ class AIDungeonAdventure:
         )
         return result['addUserToAdventure']
 
-    def request_content(self,inner):
+    async def request_content(self,inner):
         query = """
         query ($id: String) {
             content(id: $id) {
@@ -31,7 +32,7 @@ class AIDungeonAdventure:
             }
         }
         '''
-        result = self.client.request(
+        result = await self.client.request(
             query,
             variables = {
                 'id':self.id
@@ -39,8 +40,8 @@ class AIDungeonAdventure:
         )
         return result
 
-    def send_action(self,type):
-        result = self.client.request(
+    async def send_action(self,type):
+        result = await self.client.request(
             '''
             mutation ($input: ContentActionInput) {
                 sendAction(input: $input) {
@@ -59,10 +60,10 @@ class AIDungeonAdventure:
         )
         return result
 
-    def set_character_name(self,name):
+    async def set_character_name(self,name):
         if self.client.debug:
             print("Setting character name to {}".format(name))
-        result = self.client.request(
+        result = await self.client.request(
             '''
             mutation ($input: CharacterInput) {
                 updateCharacter(input: $input) {
@@ -82,8 +83,10 @@ class AIDungeonAdventure:
         self.character_name = name
         return result
 
-    def send_text(self,text,type="story"):
-        result = self.client.request(
+    async def send_text(self,text,type="story"):
+        if self.client.debug:
+            print("Sending text >{}<".format(text))
+        result = await self.client.request(
             '''
             mutation ($input: ContentActionInput) {
                 sendAction(input: $input) {
@@ -103,32 +106,32 @@ class AIDungeonAdventure:
         )
         return result
 
-    def undo(self):
+    async def undo(self):
         if self.client.debug:
             print("Requesting undo in adventure {}".format(self.public_id))
-        self.send_action('undo')
+        await self.send_action('undo')
 
-    def redo(self):
+    async def redo(self):
         if self.client.debug:
             print("Requesting redo in adventure {}".format(self.public_id))
-        self.send_action('redo')
+        await self.send_action('redo')
 
-    def retry(self):
+    async def retry(self):
         if self.client.debug:
             print("Requesting retry in adventure {}".format(self.public_id))
-        self.send_action('retry')
+        await self.send_action('retry')
 
-    def alter_action(self,action_id,text):
+    async def alter_action(self,action_id,text):
         if self.client.debug:
             print("Requesting alter action {} in adventure {}...".format(
                 action_id,
                 self.public_id)
             )
-        result = self.client.request(
+        result = await self.client.request(
             '''
-            query ($id: String) {
-                content(id: $id) {
-                    actionLoading
+            mutation ($input: ContentActionInput) {
+                doAlterAction(input: $input) {
+                    __typename
                 }
             }
             ''',
@@ -142,12 +145,12 @@ class AIDungeonAdventure:
             }
         )
 
-    def alter_memory(self, new_memory):
+    async def alter_memory(self, new_memory):
         if self.client.debug:
             print("Requesting alter memory in adventure {}...".format(
                 self.public_id)
             )
-        result = self.client.request(
+        result = await self.client.request(
             '''
             mutation ($input: ContentActionInput) {
                 updateMemory(input: $input) {
@@ -159,7 +162,7 @@ class AIDungeonAdventure:
             ''',
             variables = {
                 'input':{
-                    'text':text,
+                    'text':new_memory,
                     'type':'remember',
                     'id':self.id
                 }
@@ -167,16 +170,16 @@ class AIDungeonAdventure:
         )
         return result
 
-    def obtain_is_loading(self):
+    async def obtain_is_loading(self):
         if self.client.debug:
             print("Requesting if adventure is loading...")
-        result = self.request_content('actionLoading')
+        result = await self.request_content('actionLoading')
         return result['content']['actionLoading']
 
-    def obtain_actions(self):
+    async def obtain_actions(self):
         if self.client.debug:
             print("Requesting actions for adventure {}".format(self.public_id))
-        result = self.request_content('''
+        result = await self.request_content('''
             actions {
                 id
                 text
@@ -184,47 +187,47 @@ class AIDungeonAdventure:
         ''')
         return result['content']['actions']
 
-    def obtain_error(self):
+    async def obtain_error(self):
         if self.client.debug:
             print("Requesting error for adventure {}".format(self.public_id))
-        result = self.request_content('error')
+        result = await self.request_content('error')
         return result['content']['error']
 
-    def obtain_memory(self):
+    async def obtain_memory(self):
         if self.client.debug:
             print("Requesting memory for adventure {}".format(self.public_id))
-        result = self.request_content('memory')
+        result = await self.request_content('memory')
         return result['content']['memory']
 
-    def obtain_gamestate(self):
+    async def obtain_gamestate(self):
         if self.client.debug:
             print("Requesting gamestate for adventure {}".format(self.public_id))
-        result = self.request_content('gameState')
+        result = await self.request_content('gameState')
         return result['content']['gameState']
 
-    def obtain_mode(self):
+    async def obtain_mode(self):
         if self.client.debug:
             print("Requesting mode for adventure {}".format(self.public_id))
-        result = self.request_content('mode')
+        result = await self.request_content('mode')
         return result['content']['mode']
 
-    def obtain_has_died(self):
+    async def obtain_has_died(self):
         if self.client.debug:
             print("Requesting has died for adventure {}".format(self.public_id))
-        result = self.request_content('died')
+        result = await self.request_content('died')
         return result['content']['died']
 
-    def obtain_is_third_person(self):
+    async def obtain_is_third_person(self):
         if self.client.debug:
             print("Requesting is third person for adventure {}".format(self.public_id))
-        result = self.request_content('thirdPerson')
+        result = await self.request_content('thirdPerson')
         return result['content']['thirdPerson']
 
-    def obtain_last_action(self):
-        actions = self.obtain_actions()
+    async def obtain_last_action(self):
+        actions = await self.obtain_actions()
         return actions[-1]
 
-    def __init__(self, client, public_id):
+    async def __init__(self, client, public_id):
         self.client = client
         self.public_id = public_id
-        self.id = self.add_user_to_adventure()
+        self.id = await self.add_user_to_adventure()
--- aidclient.py
+++ aidclient.py
@@ -1,8 +1,8 @@
 from gql import gql, Client, WebsocketsTransport
 from aidadventure import AIDungeonAdventure
+from aobject import aobject
 
-
-class AIDungeonClient:
+class AIDungeonClient(aobject):
     token = ''
     gql_client = None
     debug = False
@@ -18,14 +18,14 @@ class AIDungeonClient:
             fetch_schema_from_transport=True
         )
 
-    def request(self, query_text, variables={}):
+    async def request(self, query_text, variables={}):
         query = gql(
             query_text
         )
-        return self.gql_client.execute(query,variable_values=variables)
+        return await self.gql_client.execute_async(query,variable_values=variables)
 
-    def request_user(self):
-        result = self.request(
+    async def request_user(self):
+        result = await self.request(
             """
             {
                 user {
@@ -38,8 +38,8 @@ class AIDungeonClient:
         )
         return result['user']
 
-    def request_create_anonymous_user(self):
-        result = self.request(
+    async def request_create_anonymous_user(self):
+        result = await self.request(
             """
             mutation {
                 createAnonymousAccount
@@ -53,10 +53,10 @@ class AIDungeonClient:
         )
         return result['createAnonymousAccount']
 
-    def send_event(self, eventName):
+    async def send_event(self, eventName):
         if self.debug:
             print("Sending event {}...".format(eventName))
-        result = self.request(
+        result = await self.request(
             """
                 mutation ($input: EventInput) {  sendEvent(input: $input)}
             """,
@@ -69,16 +69,16 @@ class AIDungeonClient:
         )
         return result
 
-    def get_adventure(self, id):
-        return AIDungeonAdventure(self, id)
+    async def get_adventure(self, id):
+        return await AIDungeonAdventure(self, id)
 
-    def __init__(self, token='', debug=False):
+    async def __init__(self, token='', debug=False):
         self.debug = debug
         self.set_client_with_token(token)
         if self.debug:
             print("Verifying user...")
-        if self.request_user() == None:
-            new_user = self.request_create_anonymous_user()
+        if await self.request_user() == None:
+            new_user = await self.request_create_anonymous_user()
             self.set_client_with_token(new_user['accessToken'])
             if self.debug:
                 print("Initialized with anonymous user, token {}".format(self.token))
